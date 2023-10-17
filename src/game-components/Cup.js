75744e63cb3ef9f28557b49ef0006e773e2c1a2e
Leonel Aguirre <leonel.aguirre182@gmail.com>
2023-10-17 11:34:17 -0600

    :bulb: Cup.js code cleanup.

--- src/game-components/Cup.js
+++ src/game-components/Cup.js
@@ -1,24 +1,39 @@
+// Cup's constant parameters.
+const CUP_SIZE = 100
+const CUP_BOTTOM_OFFSET = 40
+
+/**
+ * Cup class.
+ */
 class Cup {
+  /**
+   * Creates a Cup instance.
+   *
+   * @param {p5} p p5 object reference.
+   * @param {number} screenWidth the screen's width.
+   * @param {number} screenHeight the screen's height.
+   * @param {object} image cup image.
+   */
   constructor(p, screenWidth, screenHeight, image) {
     this.p = p
-
     this.screenWidth = screenWidth
     this.screenHeight = screenHeight
+    this.image = image
 
-    this.size = 100
+    // Cup's size.
+    this.size = CUP_SIZE
 
+    // Cup position vector. Initially set to the center of the screen's bottom.
     this.position = p.createVector(
       screenWidth / 2 - this.size / 2,
-      screenHeight - 40
+      screenHeight - CUP_BOTTOM_OFFSET
     )
-
-    this.image = image
   }
 
   /**
-   * Draws the object into the canvas.
+   * Cup's draw method.
    *
-   * @param {p5} p A reference to the p5 object.
+   * @param {p5} p p5 object reference.
    */
   draw(p) {
     p.push()
@@ -26,9 +41,13 @@ class Cup {
     p.pop()
   }
 
+  /**
+   * Moves cup to the right.
+   */
   moveRight() {
     let newPX = this.position.x + 10
 
+    // Limits to the right edge.
     if (newPX >= this.screenWidth - this.size) {
       this.position.x = this.screenWidth - this.size
     } else {
@@ -36,9 +55,13 @@ class Cup {
     }
   }
 
+  /**
+   * Moves cup to the left.
+   */
   moveLeft() {
     let newPX = this.position.x - 10
 
+    // Limits to the left edge.
     if (newPX <= 0) {
       this.position.x = 0
     } else {
